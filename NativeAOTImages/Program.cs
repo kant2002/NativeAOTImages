dc998c34e3ca0e0cb862ea8b5e32b3df77fd9147
Andrii Kurdiumov <kant2002@gmail.com>
2021-06-03 19:28:38 +0600

    Add project files.

---
+++ NativeAOTImages/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace NativeAOTImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 128;
+            int height = 128;
+            var file = args[0];
+            Console.WriteLine($"Loading {file}");
+            using var pngStream = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+            using var image = new Bitmap(pngStream);
+            var resized = new Bitmap(width, height);
+            using var graphics = Graphics.FromImage(resized);
+            graphics.CompositingQuality = CompositingQuality.HighSpeed;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.DrawImage(image, 0, 0, width, height);
+            resized.Save($"resized-{file}", ImageFormat.Png);
+            Console.WriteLine($"Saving resized-{file} thumbnail");
+        }
+    }
+
+}
